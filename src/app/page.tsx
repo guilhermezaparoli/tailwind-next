eae94c4ccd517cf9bfe0c1ea89b7e70764a82257
Guilherme <guilhermezapas@gmail.com>
2024-09-01 17:18:40 -0300

    FEAT: finishing sideBar, adding TabBar options with RadixUI and Framer Motion

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,10 @@
+import SettinginsTabs from '@/components/SettingsTabs'
+
 export default function Home() {
-  return <h1>teste</h1>
+  return (
+    <>
+      <h1 className="text-3xl font-medium text-zinc-900">Settings</h1>
+      <SettinginsTabs />
+    </>
+  )
 }
